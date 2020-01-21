9c456743abc076062c30cab9de4ae80996c47b50
Brandon Weaver <a7xncob@gmail.com>
2020-01-21 00:35:35 +0000

    Automatically backed up by Learn

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,6 @@ module BlogFlash
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    config.web_console.whitelisted_ips = ""
+    config.web_console.whitelisted_ips = "142.93.56.73:36388"
   end
 end
